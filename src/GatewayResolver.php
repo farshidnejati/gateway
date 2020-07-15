bf25f11eda8c57406e1aceca76fda905ab77cd87
farshid75 <nfarshidn@gmail.com>
2020-07-16 04:07:42 +0430

    change GatewayResolver file to sync with models and gateways

--- src/GatewayResolver.php
+++ src/GatewayResolver.php
@@ -2,131 +2,61 @@
 
 namespace Larabookir\Gateway;
 
-use Larabookir\Gateway\Irankish\Irankish;
-use Larabookir\Gateway\Parsian\Parsian;
-use Larabookir\Gateway\Paypal\Paypal;
-use Larabookir\Gateway\Sadad\Sadad;
-use Larabookir\Gateway\Mellat\Mellat;
-use Larabookir\Gateway\Pasargad\Pasargad;
-use Larabookir\Gateway\Saman\Saman;
+use App\User;
+use Illuminate\Support\Facades\DB;
 use Larabookir\Gateway\Asanpardakht\Asanpardakht;
-use Larabookir\Gateway\Zarinpal\Zarinpal;
-use Larabookir\Gateway\Payir\Payir;
-use Larabookir\Gateway\Exceptions\RetryException;
-use Larabookir\Gateway\Exceptions\PortNotFoundException;
 use Larabookir\Gateway\Exceptions\InvalidRequestException;
 use Larabookir\Gateway\Exceptions\NotFoundTransactionException;
-use Illuminate\Support\Facades\DB;
+use Larabookir\Gateway\Exceptions\PortNotFoundException;
+use Larabookir\Gateway\Exceptions\RetryException;
+use Larabookir\Gateway\Irankish\Irankish;
+use Larabookir\Gateway\Mellat\Mellat;
+use Larabookir\Gateway\Parsian\Parsian;
+use Larabookir\Gateway\Pasargad\Pasargad;
+use Larabookir\Gateway\Payir\Payir;
+use Larabookir\Gateway\Paypal\Paypal;
+use Larabookir\Gateway\Sadad\Sadad;
+use Larabookir\Gateway\Saman\Saman;
+use Larabookir\Gateway\Zarinpal\Zarinpal;
 
 class GatewayResolver
 {
 
-	protected $request;
+    /**
+     * @var Config
+     */
+    public $config;
+    protected $request;
+    /**
+     * Keep current port driver
+     *
+     * @var Mellat|Saman|Sadad|Zarinpal|Payir|Parsian
+     */
+    protected $port;
 
-	/**
-	 * @var Config
-	 */
-	public $config;
+    /**
+     * Gateway constructor.
+     * @param null $config
+     * @param null $port
+     */
+    public function __construct($config = null, $port = null, $user = null)
+    {
+        $this->config = app('config');
+        $this->request = app('request');
 
-	/**
-	 * Keep current port driver
-	 *
-	 * @var Mellat|Saman|Sadad|Zarinpal|Payir|Parsian
-	 */
-	protected $port;
+        if ($this->config->has('gateway.timezone'))
+            date_default_timezone_set($this->config->get('gateway.timezone'));
 
-	/**
-	 * Gateway constructor.
-	 * @param null $config
-	 * @param null $port
-	 */
-	public function __construct($config = null, $port = null)
-	{
-		$this->config = app('config');
-		$this->request = app('request');
+        if (!is_null($port)) $this->make($port, $user);
+    }
 
-		if ($this->config->has('gateway.timezone'))
-			date_default_timezone_set($this->config->get('gateway.timezone'));
-
-		if (!is_null($port)) $this->make($port);
-	}
-
-	/**
-	 * Get supported ports
-	 *
-	 * @return array
-	 */
-	public function getSupportedPorts()
-	{
-		return (array) Enum::getIPGs();
-	}
-
-	/**
-	 * Call methods of current driver
-	 *
-	 * @return mixed
-	 */
-	public function __call($name, $arguments)
-	{
-
-		// calling by this way ( Gateway::mellat()->.. , Gateway::parsian()->.. )
-		if(in_array(strtoupper($name),$this->getSupportedPorts())){
-			return $this->make($name);
-		}
-
-		return call_user_func_array([$this->port, $name], $arguments);
-	}
-
-	/**
-	 * Gets query builder from you transactions table
-	 * @return mixed
-	 */
-	function getTable()
-	{
-		return DB::table($this->config->get('gateway.table'));
-	}
-
-	/**
-	 * Callback
-	 *
-	 * @return $this->port
-	 *
-	 * @throws InvalidRequestException
-	 * @throws NotFoundTransactionException
-	 * @throws PortNotFoundException
-	 * @throws RetryException
-	 */
-	public function verify()
-	{
-		if (!$this->request->has('transaction_id') && !$this->request->has('iN'))
-			throw new InvalidRequestException;
-		if ($this->request->has('transaction_id')) {
-			$id = $this->request->get('transaction_id');
-		}else {
-			$id = $this->request->get('iN');
-		}
-
-		$transaction = $this->getTable()->whereId($id)->first();
-
-		if (!$transaction)
-			throw new NotFoundTransactionException;
-
-		if (in_array($transaction->status, [Enum::TRANSACTION_SUCCEED, Enum::TRANSACTION_FAILED]))
-			throw new RetryException;
-
-		$this->make($transaction->port);
-
-		return $this->port->verify($transaction);
-	}
-
-
-	/**
-	 * Create new object from port class
-	 *
-	 * @param int $port
-	 * @throws PortNotFoundException
-	 */
-	function make($port)
+    /**
+     * Create new object from port class
+     *
+     * @param int $port
+     * @throws PortNotFoundException
+     */
+    function make($port, $user)
     {
         if ($port InstanceOf Mellat) {
             $name = Enum::MELLAT;
@@ -152,7 +82,7 @@ class GatewayResolver
             $port = ucfirst(strtolower($port));
             $name = strtoupper($port);
             $class = __NAMESPACE__ . '\\' . $port . '\\' . $port;
-            $port = new $class;
+            $port = new $class($user);
         } else
             throw new PortNotFoundException;
 
@@ -162,5 +92,74 @@ class GatewayResolver
         $this->port->boot();
 
         return $this;
+    }
+
+    /**
+     * Get supported ports
+     *
+     * @return array
+     */
+    public function getSupportedPorts()
+    {
+        return (array)Enum::getIPGs();
+    }
+
+    /**
+     * Call methods of current driver
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+//        dd($arguments);
+        // calling by this way ( Gateway::mellat()->.. , Gateway::parsian()->.. )
+        if (in_array(strtoupper($name), $this->getSupportedPorts())) {
+            return $this->make($name, $arguments[0]);
+        }
+
+        return call_user_func_array([$this->port, $name], $arguments);
+    }
+
+    /**
+     * Callback
+     *
+     * @return $this->port
+     *
+     * @throws InvalidRequestException
+     * @throws NotFoundTransactionException
+     * @throws PortNotFoundException
+     * @throws RetryException
+     */
+    public function verify()
+    {
+        if (!$this->request->has('transaction_id') && !$this->request->has('iN'))
+            throw new InvalidRequestException;
+        if ($this->request->has('transaction_id')) {
+            $id = $this->request->get('transaction_id');
+        } else {
+            $id = $this->request->get('iN');
+        }
+
+        $transaction = $this->getTable()->whereId($id)->first();
+
+        if (!$transaction)
+            throw new NotFoundTransactionException;
+
+        if (in_array($transaction->status, [Enum::TRANSACTION_SUCCEED, Enum::TRANSACTION_FAILED]))
+            throw new RetryException;
+
+        $user = User::find($transaction->user_id);
+        $this->make($transaction->port, $user);
+
+        return $this->port->verify($transaction);
+    }
+
+    /**
+     * Gets query builder from you transactions table
+     * @return mixed
+     */
+    function getTable()
+    {
+        return DB::table($this->config->get('gateway.table'));
     }
 }
