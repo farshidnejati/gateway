3f270b90d4edfd880c489d5f24916b5c299ce9c0
farshid75 <nfarshidn@gmail.com>
2020-07-16 04:03:28 +0430

    gateway config file edited

--- config/gateway.php
+++ config/gateway.php
@@ -1,128 +1,65 @@
 <?php
 
 return [
+    
+    //--------------------------------
+    // Your users table name should be written in this section
+    //--------------------------------
+    'user_table' =>'users',
+    
+    //--------------------------------
+    // Zarinpal gateway table name
+    //--------------------------------
+    'zarinpal_gateway_table' => 'zarinpal_gateway',
 
+    //--------------------------------
+    // Mellat gateway table name
+    //--------------------------------
+    'mellat_gateway_table' => 'mellat_gateway',
+
+    //--------------------------------
+    // Saman gateway table name
+    //--------------------------------
+    'saman_gateway_table' => 'saman_gateway',
+
+    //--------------------------------
+    // Payir gateway table name
+    //--------------------------------
+    'payir_gateway_table' => 'payir_gateway',
+
+    //--------------------------------
+    // Irankish gateway table name
+    //--------------------------------
+    'irankish_gateway_table' => 'irankish_gateway',
+
+    //--------------------------------
+    // Sadad gateway table name
+    //--------------------------------
+    'sadad_gateway_table' => 'sadad_gateway',
+
+    //--------------------------------
+    // Parsian gateway table name
+    //--------------------------------
+    'parsian_gateway_table' => 'parsian_gateway',
+
+    //--------------------------------
+    // Ppasargad gateway table name
+    //--------------------------------
+    'pasargad_gateway_table' => 'pasargad_gateway',
+
+    //--------------------------------
+    // AsanPardakht gateway table name
+    //--------------------------------
+    'asanpardakht_gateway_table' => 'asanpardakht_gateway',
+    
+    
     //-------------------------------
     // Timezone for insert dates in database
     // If you want Gateway not set timezone, just leave it empty
     //--------------------------------
     'timezone' => 'Asia/Tehran',
 
-    //--------------------------------
-    // Zarinpal gateway
-    //--------------------------------
-    'zarinpal' => [
-        'merchant-id'  => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-        'type'         => 'zarin-gate',             // Types: [zarin-gate || normal]
-        'callback-url' => '/',
-        'server'       => 'germany',                // Servers: [germany || iran || test]
-        'email'        => 'email@gmail.com',
-        'mobile'       => '09xxxxxxxxx',
-        'description'  => 'description',
-    ],
-
-    //--------------------------------
-    // Mellat gateway
-    //--------------------------------
-    'mellat' => [
-        'username'     => '',
-        'password'     => '',
-        'terminalId'   => 0000000,
-        'callback-url' => '/'
-    ],
-
-    //--------------------------------
-    // Saman gateway
-    //--------------------------------
-    'saman' => [
-        'merchant'     => '',
-        'password'     => '',
-        'callback-url'   => '/',
-    ],
-
-    //--------------------------------
-    // PayIr gateway
-    //--------------------------------
-    'payir'    => [
-        'api'          => 'xxxxxxxxxxxxxxxxxxxx',
-        'callback-url' => '/'
-    ],
-
-    //--------------------------------
-    // IranKish gateway
-    //--------------------------------
-    'irankish' => [
-        'merchantId' => 'xxxxxxxxxxxxxxxxxxxx',
-        'sha1key' => 'xxxxxxxxxxxxxxxxxxxx',
-        'callback-url' => '/'
-    ],
-
-    //--------------------------------
-    // Sadad gateway
-    //--------------------------------
-    'sadad' => [
-        'merchant'      => '',
-        'transactionKey'=> '',
-        'terminalId'    => 000000000,
-        'callback-url'  => '/'
-    ],
-
-    //--------------------------------
-    // Parsian gateway
-    //--------------------------------
-    'parsian' => [
-        'pin'          => 'xxxxxxxxxxxxxxxxxxxx',
-        'callback-url' => '/'
-    ],
-    //--------------------------------
-    // Pasargad gateway
-    //--------------------------------
-    'pasargad' => [
-        'terminalId'    => 000000,
-        'merchantId'    => 000000,
-        'certificate-path'    => storage_path('gateway/pasargad/certificate.xml'),
-        'callback-url' => '/gateway/callback/pasargad'
-    ],
-
-    //--------------------------------
-    // Asan Pardakht gateway
-    //--------------------------------
-    'asanpardakht' => [
-        'merchantId'     => '',
-        'merchantConfigId'     => '',
-        'username' => '',
-        'password' => '',
-        'key' => '',
-        'iv' => '',
-        'callback-url'   => '/',
-    ],
-
-    //--------------------------------
-    // Paypal gateway
-    //--------------------------------
-    'paypal'   => [
-        // Default product name that appear on paypal payment items
-        'default_product_name' => 'My Product',
-        'default_shipment_price' => 0,
-        // set your paypal credential
-        'client_id' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-        'secret'    => 'xxxxxxxxxx_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-        'settings'  => [
-            'mode'                   => 'sandbox', //'sandbox' or 'live'
-            'http.ConnectionTimeOut' => 30,
-            'log.LogEnabled'         => true,
-            'log.FileName'           => storage_path() . '/logs/paypal.log',
-            /**
-             * Available option 'FINE', 'INFO', 'WARN' or 'ERROR'
-             *
-             * Logging is most verbose in the 'FINE' level and decreases as you
-             * proceed towards ERROR
-             */
-            'call_back_url'          => '/gateway/callback/paypal',
-            'log.LogLevel'           => 'FINE'
-
-        ]
-    ],
+    
     //-------------------------------
     // Tables names
     //--------------------------------
